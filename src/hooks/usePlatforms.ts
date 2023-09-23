1618562c1f7ff79abf35dd90f8ca8671441ff7c1
Ahmed Rashwan <rash.ahmed.2023@gmail.com>
2023-09-23 23:14:46 +0300

    Ship static data (genres, platforms)

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,4 @@
-import useData from "./useData";
+import platforms from "../data/platforms";
 
 export interface Platform {
   id: number;
@@ -7,6 +7,6 @@ export interface Platform {
   image_background: string;
 }
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () => ({ data: platforms, error: null, isLoading: null });
 
 export default usePlatforms;
